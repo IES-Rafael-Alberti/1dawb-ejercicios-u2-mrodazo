a9666d33a3b17a1375da35264bce909831a07fe8
mrodazo617 <mrodazo617@g.educaand.es>
2023-11-05 23:16:27 +0100

    Intento de hacer test del ejercicio 2.1.7 y 2.1.8 sin éxito

--- tests/test_ej2_17.py
+++ tests/test_ej2_17.py
@@ -1,5 +1,5 @@
 import pytest
-from src.ej2_17 import esFloat
+from src.ej2_17 import comprobarNumeric, caracterLimitante, convertirAFloat, tipoImpositivo
 
 @pytest.mark.parametrize(
     "entrada, expected",
@@ -7,7 +7,8 @@ from src.ej2_17 import esFloat
         ('10000', True),
         ('mujer', False),
         ('25.500', True),
+        ('12.500,50', True),
     ]
 )
-def test_esFloat_params(entrada, expected):
-    assert esFloat (entrada) == expected
+def test_comprobarNumeric_params(entrada, expected):
+    assert comprobarNumeric (entrada) == expected
